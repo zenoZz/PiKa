c51508a6ae1a54c3c42bf52cffd7710b852dae33
zeno <77849093@qq.com>
2016-09-08 17:00:00 +0800

    *前台样式

--- resources/views/post/rightMenu.blade.php
+++ resources/views/post/rightMenu.blade.php
@@ -19,25 +19,6 @@
     </div>
     <!-- end tag cloud widget -->
 
-    <!-- start widget -->
-    <div class="widget">
-        <h4 class="title">搬砖地址</h4>
-        <div class="content download">
-            <a class="btn btn-default btn-block" role="button" data-toggle="collapse" href="#phpLink" aria-expanded="false" aria-controls="phpLink">
-                LINK
-            </a>
-            <div class="collapse" id="phpLink">
-                <div class="well">
-                    <p><a href="http://blog.snsgou.com/post-42.html" class="btn btn-default btn-block" target="_blank">PHP反射入门教程</a></p>
-                    <p><a href="http://www.cnblogs.com/taletao/p/4212916.html" class="btn btn-default btn-block" target="_blank">PHP 资源大全</a></p>
-                    <p><a href="http://laravelbase.com/" target="_blank" class="btn btn-default btn-block">laravelbase</a></p>
-                    <p><a href="http://laravel.so/" target="_blank" class="btn btn-default btn-block">laravel.so</a></p>
-                    <p><a href="http://laravelacademy.org/" target="_blank" class="btn btn-default btn-block">laravel学院</a></p>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <!-- start tag cloud widget -->
     <div class="widget">
         <h4 class="title">标签云</h4>
