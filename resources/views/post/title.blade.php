8b168ed02d07eb535f344b1488c5c786650ee84a
zeno_zz <77849093@qq.com>
2016-12-12 10:15:58 +0800

    *链接

--- resources/views/post/title.blade.php
+++ resources/views/post/title.blade.php
@@ -3,7 +3,7 @@
         <!--标头-->
         <div class="col-sm-12 text-center" style="margin-top: 35px;">
             {{--<input type="" class="form-control" placeholder="">--}}
-            <h1 style="color: #ffffff"><span class="hide">轻 - </span>轻  <a href="javascript:;"><small style="color: #ffffff">&nbsp;对我而言</small></a></h1>
+            <h1 style="color: #ffffff"><span class="hide">轻 - </span>轻  <a target="_blank" href="http://weibo.com/2962035201/profile?topnav=1&wvr=6&is_all=1"><small style="color: #ffffff">&nbsp;对我而言</small></a></h1>
             {{--<form action="{{ route('post.index') }}" method="get">--}}
                 {{--<div class="input-group text-center">--}}
                     {{--<input type="text" class="form-control">--}}
