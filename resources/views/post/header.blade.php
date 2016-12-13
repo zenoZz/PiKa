20e3e56b4b9d02edeb5db07284362feaf0ca3401
zeno_zz <77849093@qq.com>
2016-12-13 12:58:45 +0800

    *首页

--- resources/views/post/header.blade.php
+++ resources/views/post/header.blade.php
@@ -4,8 +4,9 @@
         <div class="row">
             <div class="col-xs-12" style="text-align: center">
                 <ul class="nav nav-list">
-                    <li><a href="/">首页</a></li>
+
                     <li><a href="{{ route('article.list', ['type' => 1]) }}">左手代码</a></li>
+                    <li><a href="/"><strong>/</strong></a></li>
                     <li><a href="{{ route('article.list', ['type' => 2]) }}">右手诗</a></li>
                 </ul>
             </div>
